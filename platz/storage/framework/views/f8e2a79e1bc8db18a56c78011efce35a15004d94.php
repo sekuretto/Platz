e0e20462a028145fc7b71dc4524d96bdde6d1078
K8237 <k8237@student.jamk.fi>
2018-03-13 10:27:09 +0200

    style

---
+++ platz/storage/framework/views/f8e2a79e1bc8db18a56c78011efce35a15004d94.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+
+   <head>
+      <title>Laravel template by late almost</title>
+   </head>
+
+   <body>
+      <h1>Laravel template by late almost</h1>
+<hr>
+	<?php echo $__env->yieldContent('content'); ?>
+<hr>
+
+<?php echo $__env->yieldContent('footer'); ?>
+
+   </body>
+
+</html>
