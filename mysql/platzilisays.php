d48a940861037635a0f3a68d5c092513310e1728
K1559 <k1559@student.jamk.fi>
2018-03-13 10:45:30 +0200

    Update platzilisays.php

--- mysql/platzilisays.php
+++ mysql/platzilisays.php
@@ -14,7 +14,7 @@ if (!$conn) {
 echo "Connected successfully <br>";
 
 $sql = "INSERT INTO KAYTTAJA (kayttajanimi, salasana, email, kaupunki)
-VALUES ('Koljonen', MD5('kikuli'), 'timppa@gwyneth.com', 'johannes')";
+VALUES ('nimi', MD5('salasana'), 'sähköposti@sposti.com', 'kaupunki')";
 
 if ($conn->query($sql) === TRUE) {
     echo "New record created successfully";
