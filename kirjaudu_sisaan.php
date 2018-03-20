44ee9f2eb007d6175d090e5587e7221ff542da36
Noora Jokinen <K8241@Labranet.jamk.fi>
2018-03-20 08:29:14 +0200

    kirjaudu sisään

--- kirjaudu_sisaan.php
+++ kirjaudu_sisaan.php
@@ -10,11 +10,13 @@ Kirjaudu sisään
     <div class="modal-content">
       <div class="modal-header">
         <h5 class="modal-title">Kirjaudu sisään</h5>
+          
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
       </div>
       <div class="modal-body">
+          <p>Lisätäksesi ilmoituksen, sinun tulee kirjautua sisään. Jos sinulla ei ole tunnuksia, voit luoda ne rekisteröitymällä.</p>
           <form action="#" method="post" enctype="multipart/form-data">
               <div class="form-group col-md-6">
                      <label for="staticEmail">Käyttäjätunnus </label>
