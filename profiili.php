c2c0070a56e5ad728a7a7fe732dea3c7d053bec0
K8691 <k8691@student.jamk.fi>
2018-03-27 09:42:16 +0300

    profiili.php

--- profiili.php
+++ profiili.php
@@ -158,7 +158,7 @@
     </div>
     
     <!-- käyttäjän poisto -->
-    <div class="modal hide fade" id="#poistakayttaja" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+    <div class="modal hide fade" id="poistakayttaja" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered" role="document">
         <div class="modal-content">
         <div class="modal-header">
