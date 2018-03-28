850ba2cc24e3087258cf42d5dbac2b7bcd47b64a
Noora Jokinen <K8241@LABRANET.JAMK.FI>
2018-03-28 10:44:44 +0300

    mo

--- luo_tunnus.php
+++ luo_tunnus.php
@@ -1,5 +1,6 @@
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 <meta charset="utf-8">
+<title>MOMOMOMOM</title>
 
 <div class="modal hide fade" id="luotunnus" tabindex="-1" role="dialog" data-focus-on="input:first">
     <div class="modal-dialog" role="document">
