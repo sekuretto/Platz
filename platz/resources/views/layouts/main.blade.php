8961a54e72994275d992a2c6284981be6432bd9c
Lauri Girsén <K8297@Labranet.jamk.fi>
2018-04-04 10:03:40 +0300

    profiililinkki toimii

--- platz/resources/views/layouts/main.blade.php
+++ platz/resources/views/layouts/main.blade.php
@@ -26,7 +26,7 @@
                     <a class="col" data-toggle="modal" data-target="#kirjaudu" style="margin-left: 100px">Kirjaudu sisään</a>
                     <a class="col" data-toggle="modal" data-target="#register" style="margin-left: 100px">Luo tunnus</a>
                 @else
-                    <a class="col" data-toggle="modal" href="/{{ Auth::user()->name }}" style="margin-left: 100px">Profiili</a>
+                    <a class="col" href="/{{ Auth::user()->name }}" style="margin-left: 100px">Profiili</a>
                     <a class="col" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();" style="margin-left: 100px">Kirjaudu ulos</a>    
                     <a class="col" data-toggle="modal" data-target="#ilmoitus" style="margin-left: 100px">Lisää ilmoitus</a>
                         <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
