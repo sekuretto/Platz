aa2373e06946ba61f19c30bcae4dfc841cfc0d44
Lauri Girsén <K8297@LABRANET.JAMK.FI>
2018-04-11 11:11:07 +0300

    kuva toimii profiilissa

--- platz/resources/views/welcome.blade.php
+++ platz/resources/views/welcome.blade.php
@@ -12,7 +12,7 @@
             <p class="box">{{$post->body}}<br>
             <b>Sähköposti: {{$post->user->email}}</b><br>
             <b>Kotikaupunki: {{$post->user->city}}</b><br>
-            <img src="storage/{{$post->user->name}}/{{$post->image}}" alt="kuva">
+            <img src="/storage/{{$post->user->name}}/{{$post->image}}" alt="kuva">
             </p>
             
             </article>
