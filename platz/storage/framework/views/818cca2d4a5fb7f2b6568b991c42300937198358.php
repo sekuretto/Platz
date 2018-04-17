6741daf6a7675b219c25cad81c08d4d30eaedf4b
Lauri Girsén <K8297@LABRANET.JAMK.FI>
2018-04-17 10:14:31 +0300

    h

--- platz/storage/framework/views/818cca2d4a5fb7f2b6568b991c42300937198358.php
+++ platz/storage/framework/views/818cca2d4a5fb7f2b6568b991c42300937198358.php
@@ -1,4 +1,3 @@
-<?php $__env->startSection('content'); ?>
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
@@ -44,6 +43,3 @@
         </div>
     </div>
 </div>
-<?php $__env->stopSection(); ?>
-
-<?php echo $__env->make('layouts.app', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
