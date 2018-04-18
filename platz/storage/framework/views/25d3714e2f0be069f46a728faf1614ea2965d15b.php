cc5f2324e66f5dbd23b31a4ae1e663c69466589f
Noora Jokinen <K8241@LABRANET.JAMK.FI>
2018-04-18 11:48:12 +0300

    je

--- platz/storage/framework/views/25d3714e2f0be069f46a728faf1614ea2965d15b.php
+++ platz/storage/framework/views/25d3714e2f0be069f46a728faf1614ea2965d15b.php
@@ -28,48 +28,59 @@
                 <?php endif; ?>
                 <p><b>Sähköposti: <?php echo e($post->user->email); ?></b><br></p>
                 <p><b>Kotikaupunki: <?php echo e($post->user->city); ?></b></p> 
-                <a data-toggle="modal" data-target="#ilmianna">Ilmianna ilmoitus</a>
+                <a data-toggle="modal" data-target="#<?php echo e($post->id); ?>" style="cursor:pointer;text-decoration:underline">Ilmianna ilmoitus</a>
             </div>
             </article>
             <br>
+    <div class="modal" id="<?php echo e($post->id); ?>" tabindex="-1" role="dialog">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title">Ilmianna ilmoitus: <?php echo e($post->title); ?></h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div class="modal-body">
+                  <?php echo Form::open(['action' => 'PagesController@report', 'method' => 'POST', 'enctype' => 'multipart/form-data']); ?>
+
+
+                            <div class="form-group">
+                                <?php echo e(Form::label('reason', 'Valitse ilmiannon tyyppi')); ?>
+
+                <?php echo e(Form::select('reason', ['Valitse' => 'Valitse', 'Laiton' => 'Laiton', 'Sopimaton' => 'Sopimaton', 'Vilpillinen' => 'Vilpillinen', 'Muu syy' => 'Muu syy'])); ?>
+
+                            </div>
+                            <div class="form-group">
+                                <?php echo e(Form::label('body', 'Lisätietoja')); ?>
+
+                                <?php echo e(Form::textarea('body', '', ['class' => 'form-control'])); ?>
+
+                                <?php echo e(Form::textarea('id', $post->id, ['style' => 'display:none'])); ?>
+
+                                <?php echo e(Form::textarea('title', $post->title, ['style' => 'display:none'])); ?>
+
+                                <?php echo e(Form::textarea('postBody', $post->body, ['style' => 'display:none'])); ?>
+
+                            </div>
+                  
+
+              </div>
+              <div class="modal-footer">
+                  <?php echo e(Form::submit('Lähetä ilmianto', ['class' => 'btn btn-primary'])); ?>
+
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Peruuta</button>
+              </div>
+                <?php echo Form::close(); ?>
+
+            </div>
+      </div>
+</div>
         <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
     <?php else: ?>
         <p>NO POSTS</p>
     <?php endif; ?>
-    <div class="modal" id="ilmianna" tabindex="-1" role="dialog">
-    <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title">Ilmianna</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-          <form>
-                    <div class="form-group col-md-6">
-                        <label for="inputState">Valitse ilmiannon tyyppi</label>
-                        <select id="inputState" class="form-control">
-                            <option selected>Valitse vaihtoehdoista</option>
-                            <option>Laiton</option>
-                            <option>Sopimaton</option>
-                            <option>Vilpillinen</option>
-                            <option>Muu syy</option>
-                        </select>
-                    </div>
-                    <div class="form-group col-md-5">
-                        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" placeholder="Lisätietoja"></textarea>
-                    </div>
-            </form>
 
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-primary">Lisää ilmoitus</button>
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Peruuta</button>
-      </div>
-    </div>
-  </div>
-</div>
 <?php $__env->stopSection(); ?>
 <?php $__env->startSection('nav'); ?>
 <div class="menu" id="myTopnav">
@@ -90,6 +101,4 @@
 
 </div>  
 <?php $__env->stopSection(); ?>
-    
-
 <?php echo $__env->make('layouts.main', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
